6e61ebb7658d91d794bcf95c8bb2f06c626dbce8
Sahrial Ardians <sahrialardians@gmail.com>
2025-08-13 11:55:20 +0700

    Set a dummy data, and use component in this page

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,51 @@
-import Image from "next/image";
+import EmptyProduct from "@/components/EmptyProduct";
+import ProductItems from "@/components/ProductItems";
+import StatsOverview from "@/components/StatsOverview";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const data = [
+    { id: 1, name: "Suace teriyaki", category: "Ingredients", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 June, 2025" },
+    { id: 2, name: "Sauce tomato", category: "Ingredients", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 Junly, 2025" },
+    { id: 3, name: "Suace tiram", category: "Ingredients", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 June, 2025" },
+    { id: 4, name: "Sauce BBQ", category: "Ingredients", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 June, 2025" },
+    { id: 5, name: "Fresh Milk", category: "Dairy", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 July, 2025" },
+    { id: 6, name: "Heavy Milk", category: "Dairy", qty: 1, unit: "liters", isFresh: true, expiredDate: "21 June, 2025" },
+    { id: 7, name: "Yogurt", category: "Dairy", qty: 1, unit: "bottles", isFresh: true, expiredDate: "21 June, 2025" }
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const limit = 10;
+
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <div className="px-2">
+        {/* Stats Overview */}
+        <StatsOverview />
+
+        {/* Recent Product */}
+        <section className="px-2 flex-1">
+          <div className="flex items-center justify-between mb-2">
+            <h2 className="text-lg font-semibold text-gray-900">Recent Products</h2>
+            <Link
+              href="/inventory"
+              className="text-xs text-gray-700 hover:text-blue-700"
+            >
+              See all
+            </Link>
+          </div>
+
+          <div className="space-y-3 mb-20">
+            {data.length > 0 ? (
+              (data ?? []).slice(0, limit).map((item) => (
+                <ProductItems key={item.id} product={item} />
+              ))
+              ) : (
+                <EmptyProduct />
+              )
+            }
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
