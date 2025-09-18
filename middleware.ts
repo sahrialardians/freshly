bd52486a4fb2b5ce162808c74313ee413655c5d9
Sahrial Ardians <sahrialardians@gmail.com>
2025-09-18 15:20:54 +0700

    fix: absolute URLs on middleware

--- middleware.ts
+++ middleware.ts
@@ -6,7 +6,11 @@ const isPublicRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  if (!isPublicRoute(req)) await auth.protect();
+  if (!isPublicRoute(req)) {
+    await auth.protect({
+      unauthenticatedUrl: new URL("/sign-in", req.url).toString()
+    });
+  }
 })
 
 export const config = {
